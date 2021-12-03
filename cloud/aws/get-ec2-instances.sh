0d9fa8b14a0cb6dca3f85453c6a708b614ad74b2
luismiguel.saez <luismiguelsaez83@gmail.com>
2021-12-03 16:11:07 +0100

    Result into variable

--- cloud/aws/get-ec2-instances.sh
+++ cloud/aws/get-ec2-instances.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
-PROFILE=$1
+JSON=$(aws ec2 $@ describe-instances 2>&1)
 
-aws ec2 --profile ${PROFILE} describe-instances | jq -r '.Reservations[].Instances[]|[(.Tags[]|select(.Key == "Name")|. .Value),.ImageId,.InstanceType,.NetworkInterfaces[].PrivateIpAddress,.KeyName]|@csv' | sed 's/"//g' | column -t -s,
+[ $? -ne 0 ] && echo "Error retrieving instances [$?]: $JSON"
+
+echo $JSON | jq -r '.Reservations[].Instances[]|[(.Tags[]|select(.Key == "Name")|. .Value),.ImageId,.InstanceType,.NetworkInterfaces[].PrivateIpAddress,.KeyName]|@csv' | sed 's/"//g' | column -t -s,
